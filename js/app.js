a004369f98180a7da9761d8d8c9d848b1ba34dbc
Doron <devdoron@gmail.com>
2015-06-11 23:37:03 -0700

    added time

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-$("#result").load("https://www.reddit.com/r/EarthPorn+wallpaper+MinimalWallpaper+spaceporn/search?q=1920+1080&sort=top&restrict_sr=on&t=week .thing.link", function(result) {
-    var obj = $(this).find('#result'), html = obj.html(); $(this).append($('<div>').text(html)); });
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
 
+function get12Hour(i) {
+    return i % 12;   
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    h = get12Hour(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+startTime();
