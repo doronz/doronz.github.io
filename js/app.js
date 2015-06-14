02967d1c504e86cf447551e0d7ebf9f5e81ef89e
doronz <doronz@users.noreply.github.com>
2015-06-13 18:51:28 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function loadTemp() {
     var data;
     
     $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-        $('#weather').html(Math.round(data.currently.temperature)) + \xB0;
+        $('#weather').html(Math.round(data.currently.temperature)) + '\xB0';
     });
     }
 loadTemp();
