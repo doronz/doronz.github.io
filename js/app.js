6bf38a0dece9634a294de2c3e2482c1b5395fc91
doronz <doronz@users.noreply.github.com>
2015-06-14 18:37:51 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -53,5 +53,5 @@ var wallpaper = ["http://i.imgur.com/DuMqW96.jpg", "http://i.imgur.com/V0n6d41.j
 function loadNewWallpaper() {
     console.log("Loading wallpaper");
     var random = Math.floor(Math.random() * wallpaper.length);
-    $("body").css("url(wallpaper[random])");
+    $("body").css("background-image", "url(wallpaper[random])");
 }
