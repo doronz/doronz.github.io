365820d11ab167f83ca2d21914d4c1a16a7d6d7f
doronz <doronz@users.noreply.github.com>
2015-06-14 19:11:54 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ function loadNewWallpaper() {
     var random = Math.floor(Math.random() * wallpaper.length);
     var chosen = wallpaper[random];
     console.log("Set to: " + chosen);
-    $('body').fadeTo('slow', 0.3, function() {
+    $('body').fadeTo('slow', 1, function() {
         $(this).css('background-image', 'url(' + chosen + ')');
     }).delay(1000).fadeTo('slow', 1);
 }
